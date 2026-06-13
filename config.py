d6504f13e2438a45e712dbe3f4f444ceb4150334
Kasi Lokavardhan <kasilokavardhan200@gmail.com>
2026-06-13 15:43:07 +0530

    Added config and speech Engine

---
+++ config.py
@@ -0,0 +1,10 @@
+WAKE_WORD = 'steve'
+LANGUAGE = 'en-US'
+TTS_RATE = 175
+TTS_VOLUME = 1.0
+TIMEOUT = 5
+PHRASE_LIMIT = 10
+
+GREET_MSG = 'Hello! I am Steve. How can I help you?'
+WAKE_MSG = 'Yes? I am listening.'
+ERROR_MSG = 'Sorry, I did not understand that.'
